dfbc5e314efeb775379ae3efb3586129006a5500
Reinaldo NUnes <reinaldo.nunes2@gmail.com>
2022-10-18 23:22:32 -0300

    Feat: getting query selector to customize with js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import "./css/index.css"
 
-const colors = {
-  visa: ["#436D99", "#2D57F2"],
-  mastercard: ["#DF6F29", "#C69347"],
-  default: ["black", "gray"],
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccIconFlag = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(flag) {
+  const theme = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    nubank: ["#A332B3", "#530082"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", theme[flag][0])
+  ccBgColor02.setAttribute("fill", theme[flag][1])
+  ccIconFlag.setAttribute("src", `cc-${flag}.svg`)
 }
 
-const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
-ccBgColor01.setAttribute("fill", "green")
-
-const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
-ccBgColor02.setAttribute("fill", "red")
+globalThis.setCardType = setCardType
