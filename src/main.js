0343741fc96d7407deaf5209814d7ceead52bd5f
Reinaldo NUnes <reinaldo.nunes2@gmail.com>
2022-10-18 23:07:46 -0300

    Feat: adding custom styles

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./css/index.css"
+
+const colors = {
+  visa: ["#436D99", "#2D57F2"],
+  mastercard: ["#DF6F29", "#C69347"],
+  default: ["black", "gray"],
+}
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+ccBgColor01.setAttribute("fill", "green")
+
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+ccBgColor02.setAttribute("fill", "red")
